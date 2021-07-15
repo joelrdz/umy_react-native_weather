54de4ecaf896eacd5f78432ae7e566388c65d529
Joel Rdz <joelrdzh@gmail.com>
2021-07-15 10:01:48 -0500

    Picker with country list and basic styles

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Form from './components/Form';
 
 const App = () => {
   return (
     <>
-      <Form />
+      <View style={styles.app}>
+        <View style={styles.container}>
+          <Form />
+        </View>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71, 149, 212)',
+    justifyContent: 'center'
+  },
+  container: {
+    marginHorizontal: '2.5%'
+  }
 });
 
 export default App;
--- components/Form.js
+++ components/Form.js
@@ -1,27 +1,63 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableWithoutFeedback } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 const Form = () => {
   return (
     <>
       <View>
-        <View style={StyleSheet.form}>
+        <View>
           <TextInput
+            style={styles.input}
             placeholder='Ciudad'
             placeholderTextColor='#666'
           />
         </View>
+        <View>
+          <Picker
+            itemStyle={{ height: 120, backgroundColor: '#fff' }}
+          >
+            <Picker.Item label='-- Selecciona un país --' value='' />
+            <Picker.Item label='Estados Unidos' value='US' />
+            <Picker.Item label='México' value='MX' />
+            <Picker.Item label='Argentina' value='AR' />
+            <Picker.Item label='Colombia' value='CO' />
+            <Picker.Item label='Costa Rica' value='CR' />
+            <Picker.Item label='España' value='ES' />
+            <Picker.Item label='Perú' value='PE' />
+          </Picker>
+        </View>
+        <TouchableWithoutFeedback>
+          <View style={styles.btnSearch}>
+            <Text style={styles.txtSearch}>Buscar Clima</Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  form: {
-    marginTop: 100,
-    width: '60%',
-    marginHorizontal: 'auto'
+  input: {
+    height: 50,
+    backgroundColor: '#fff',
+    padding: 10,
+    marginBottom: 20,
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  btnSearch: {
+    marginTop: 50,
+    backgroundColor: '#000',
+    padding: 10
+  },
+  txtSearch: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontSize: 18
   }
-})
+});
 
 export default Form;
